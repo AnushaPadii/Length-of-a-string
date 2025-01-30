fac263102f7f36de4ae124c7bf4cd5983195337f
AnushaPadii <anushapadi142@gmail.com>
2025-01-30 22:08:00 +0530

    Add files via upload

---
+++ Stringlength.java
@@ -0,0 +1,8 @@
+public class Stringlength {
+    public static void main(String[] args) {
+        String str="Java";
+        int length=str.length();
+        System.out.println("length:"+length);
+    }
+    
+}
